c0e56dfd1144cfa9da8696730ffa6e087d3a6d7c
GitLabSJ <aeeou12@gmail.com>
2023-04-30 16:51:03 +0900

    feat: 라우팅,Header 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Routes/Root";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 function App() {
-  return null;
+  const client = new QueryClient();
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
